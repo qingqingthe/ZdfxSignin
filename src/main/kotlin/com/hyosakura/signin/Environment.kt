84fcad938d0950ef2bdee477d8a7cd2364d4d051
LovesAsuna <qq625924077@gmail.com>
2021-11-12 18:53:32 +0800

    :zap: refactor(util): get env

---
+++ src/main/kotlin/com/hyosakura/signin/Environment.kt
@@ -0,0 +1,21 @@
+package com.hyosakura.signin
+
+object Environment {
+    lateinit var cookieMap : Map<String, String>
+
+    private fun loadCookie() {
+        cookieMap = System.getenv().mapKeys {
+            it.key.uppercase()
+        }.filter {
+            it.key.endsWith("_COOKIE")
+        }
+    }
+
+    fun getCookie(key: String) : String? {
+        return cookieMap[key.uppercase()]
+    }
+
+    init {
+        loadCookie()
+    }
+}
