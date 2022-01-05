9c36fa8853b48ac55dba21fa54a7402edcd5ed0b
LovesAsuna <qq625924077@gmail.com>
2022-01-05 21:06:37 +0800

    :zap: refactor(feat): use selenium

--- src/main/kotlin/com/hyosakura/signin/sign/forum/discuz/Zdfx.kt
+++ src/main/kotlin/com/hyosakura/signin/sign/forum/discuz/Zdfx.kt
@@ -3,6 +3,18 @@ package com.hyosakura.signin.sign.forum.discuz
 import com.hyosakura.signin.sign.Response
 import com.hyosakura.signin.sign.Result
 import com.hyosakura.signin.util.Request
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.openqa.selenium.By
+import org.openqa.selenium.Cookie
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
+import java.time.Clock
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.util.*
 
 /**
  * @author LovesAsuna
@@ -16,10 +28,43 @@ open class Zdfx(cookie: String) : Discuz(cookie) {
     }
 
     private suspend fun lottery(cookie: String): Response {
-        val lotteryUrl = "${baseUrl}plugin.php?id=yinxingfei_zzza:yaoyao"
-        val response =
-            Request.submitForm(lotteryUrl, mapOf("formhash" to formHash), headers = mapOf("Cookie" to cookie))
-        return getText(response, "#res", "#res")
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe")
+        val driver = ChromeDriver()
+        driver.manage().window().maximize()
+        driver.get("${baseUrl}k_misign-sign.html")
+        cookie.split(";").forEach {
+            val entry = it.split("=")
+            driver.manage().addCookie(
+                Cookie(
+                    entry[0].trim(), entry[1].trim(), "bbs.zdfx.net", "/", Date.from(
+                        LocalDateTime.now().plusDays(1).toInstant(
+                            ZoneOffset.UTC
+                        )
+                    )
+                )
+            )
+        }
+         fun getWait(timeout: Duration): WebDriverWait {
+            return WebDriverWait(
+                driver,
+                timeout,
+                Duration.ofMillis(500L),
+                Clock.systemDefaultZone()
+            ) { duration ->
+                runBlocking {
+                    delay(duration.toMillis())
+                }
+            }
+        }
+        driver.navigate().refresh()
+        driver.get("${baseUrl}plugin.php?id=yinxingfei_zzza:yaoyao")
+        val button = driver.findElement(By.cssSelector(".num_box > .btn"))
+        getWait(Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable(button))
+        button.click()
+        val res = driver.findElement(By.cssSelector("#res"))
+        getWait(Duration.ofSeconds(5)).until(ExpectedConditions.textToBePresentInElement(res, "已经"))
+        driver.quit()
+        return true to res.text
     }
 
     private suspend fun forumSign(cookie: String): Response {
