c3bc635afeec8ff13610c7e20e9e373fa69fc50b
LovesAsuna <qq625924077@gmail.com>
2022-04-18 16:14:25 +0800

    :recycle: refactor(struct): complete the refactoring of huahuo

---
+++ forum/huahuo.go
@@ -0,0 +1,68 @@
+package forum
+
+import (
+	"github.com/LovesAsuna/ForumSignin/util"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type Huahuo struct {
+	name,
+	baseUrl,
+	cookie string
+}
+
+func (huahuo *Huahuo) FormHash() (string, bool) {
+	return FormHash(huahuo)
+}
+
+func (huahuo *Huahuo) Name() string {
+	return huahuo.name
+}
+
+func (huahuo *Huahuo) BasicUrl() string {
+	return huahuo.baseUrl
+}
+
+func (huahuo *Huahuo) Cookie() string {
+	return huahuo.cookie
+}
+
+func (huahuo *Huahuo) Sign() (<-chan string, bool) {
+	signUrl := huahuo.baseUrl + "plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&inajax=1"
+	data := make(url.Values)
+	hashChannel := make(chan string)
+	go func() {
+		hash, ok := huahuo.FormHash()
+		if !ok {
+			hashChannel <- ""
+		} else {
+			hashChannel <- hash
+		}
+	}()
+	data["qdxq"] = []string{"kx"}
+	data["qdmode"] = []string{"1"}
+	data["todaysay"] = []string{"签到"}
+	data["fastreply"] = []string{"0"}
+	hash := <-hashChannel
+	if len(hash) == 0 {
+		return nil, false
+	}
+	data["formhash"] = []string{hash}
+	req, err := http.NewRequest("POST", signUrl, strings.NewReader(data.Encode()))
+	if err != nil {
+		return nil, false
+	}
+	req.Header.Set("Cookie", huahuo.cookie)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	c := make(chan string)
+	go func() {
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			c <- err.Error()
+		}
+		c <- util.GetText(res, "div.c", "div.c", true)
+	}()
+	return c, true
+}
