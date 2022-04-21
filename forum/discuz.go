a1bef3d9d71f7dce495523214d71401965601ece
LovesAsuna <qq625924077@gmail.com>
2022-04-21 16:19:51 +0800

    :bug: fix(request): modify header

--- forum/discuz.go
+++ forum/discuz.go
@@ -13,7 +13,9 @@ type Discuz interface {
 func FormHash(discuz Discuz) (string, bool) {
 	req, _ := http.NewRequest("GET", discuz.BasicUrl(), nil)
 	req.Header.Set("Cookie", discuz.Cookie())
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36")
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Accept-Charset", "UTF-8")
 	resp, err := http.DefaultClient.Do(req)
 	defer resp.Body.Close()
 	if err != nil {
